ffb0ad9369ace648ba5000641344d1e9be06a6a0
markochen <chenmarko77@gmail.com>
2024-10-14 12:07:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+// const message = 'Welcome to Bahamas!';
+// Розбиває рядок по розмежовувачу, в такому випадку це пробіл
+// console.log(message.split(' ')[1].toUpperCase());//["Welcome","to", "Bahamas!"]
+// console.log(message.split(' ')());
+// const splitMessage = message.split(' ')
+// console.log(splitMessage[1].toUpperCase());
+// console.log(splitMessage);
+// console.log(splitMessage.join('-'));
+
+// const clients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+// console.log(clients.indexOf('Poly')); // 2
+// console.log(clients.indexOf('Monkong')); // -1
+// console.log(clients.includes('Poly')); // true
+// console.log(clients.includes('Monkong')); // false
+
+// clients.push('Marko')
+// clients.pop()
+// clients.unshift('24')
+// clients.shift()
+// console.log(clients);
+
+// const clients = [1, 2, 3, 4, 5];
+// clients.push(6)
+// console.log(clients);
+
+// const clients = ['Mango', 'Ajax', 'Poly'];
+// clients.pop()
+// console.log(clients);
+
+// const clients = [4421, 24242, 3, 424, 535445];
+// console.log(clients.indexOf(3)); 
+
+//  const clients = ['Mango', 'Ajax', 'Poly', 'Kiwi','marko'];
+ //  console.log(clients.join(','));
+
+// const uniqueClients = clients.slice(1,3)
+// console.log(uniqueClients);
+// console.log(clients);
+
+// const copyClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+// const fullClas = clients.concat(copyClients)
+// console.log(fullClas);
+
+// const title = 'Top 10 benefits of React framework';
+// let titleNew = title.toLowerCase() 
+// titleNew = titleNew.split(' ')
+// console.log(titleNew);
+// console.log(titleNew.join('-'));
+
+// const array1 = [5, 10, 15, 20];
+// const array2 = [10, 20, 30];
+
+// const array = array1.concat(array2)
+// console.log(array);
+
+// let sum = 0
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//   sum = sum + array[index]   
+// }
+// console.log(sum);
+
+// const message = 'Welcome to Ukraine!'
+// let messageOne = message.split('')
+// console.log(messageOne);
+// console.log(messageOne.indexOf('l'));
+// messageOne = messageOne.join()
+// console.log(messageOne);
+
+ 
+
+// const players = ['Mango', 'Ajax', 'Poly', 'Kiwi','marko'];
+
+// for (let index = 0; index < array.length; index++) {
+//     players[index] = players[index]+'-'+ Number(index+1)
+// }
+// console.log(players);
